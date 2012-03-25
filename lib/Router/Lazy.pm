9b2028e0b23125158ca51a29dfbe6b473fa496af
punytan <punytan@gmail.com>
2012-03-25 19:13:55 +0900

    initial commit

---
+++ lib/Router/Lazy.pm
@@ -0,0 +1,32 @@
+package Router::Lazy;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Router::Lazy -
+
+=head1 SYNOPSIS
+
+  use Router::Lazy;
+
+=head1 DESCRIPTION
+
+Router::Lazy is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
